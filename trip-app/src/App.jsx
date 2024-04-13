75bea137047c0ba6ab27ad85bdc1faf900902552
devindaJJ <it22363534@my.sliit.lk>
2024-04-13 23:14:26 +0530

    create pages and routes

--- trip-app/src/App.jsx
+++ trip-app/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import React from 'react';
+import TripCreate from './pages/TripCreate';
+import TripUpdate from './pages/TripUpdate';
+import TripDelete from './pages/TripDelete';
+import TripView from './pages/TripView';
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-green-500'>App</h1>
+    <BrowserRouter>
+              <Routes>
+                    <Route path="/tripcreate" element={<TripCreate />} />
+                    <Route path="/tripupdate" element={<TripUpdate />} />
+                    <Route path="/tripview" element={<TripView />}  />
+                    <Route path="/tripdelete" element={<TripDelete />} />
+              </Routes>
+    </BrowserRouter>
   )
 }
---
+++ trip-app/src/pages/TripDelete.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TripDelete() {
+  return (
+    <div>TripDelete</div>
+  )
+}
