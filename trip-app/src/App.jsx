1736ab2e63827a0fc364662c91379275aa9e59f6
devindaJJ <it22363534@my.sliit.lk>
2024-04-14 09:51:20 +0530

    create header section

--- trip-app/src/App.jsx
+++ trip-app/src/App.jsx
@@ -1,18 +1,26 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react';
+import Home from './pages/Home';
 import TripCreate from './pages/TripCreate';
 import TripUpdate from './pages/TripUpdate';
 import TripDelete from './pages/TripDelete';
 import TripView from './pages/TripView';
+import Header from './components/Header';
+import AboutUs from './pages/AboutUs';
+
 
 export default function App() {
   return (
     <BrowserRouter>
+
+    <Header/>
               <Routes>
-                    <Route path="/tripcreate" element={<TripCreate />} />
+                    <Route path="/" element={<Home/>} />
+                    <Route path="tripcreate" element={<TripCreate />} />
                     <Route path="/tripupdate" element={<TripUpdate />} />
                     <Route path="/tripview" element={<TripView />}  />
                     <Route path="/tripdelete" element={<TripDelete />} />
+                    <Route path="aboutus" element={<AboutUs/>} />
               </Routes>
     </BrowserRouter>
   )
---
+++ trip-app/src/components/Header.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import { Link, useLocation } from "react-router-dom";
+
+const Header = () => {
+  const path = useLocation().pathname;
+  return (
+    <header className="bg-gray-800 p-4">
+      <nav className="flex items-center justify-between">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg
+            className="h-8 w-8 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            />
+          </svg>
+          <span className="font-semibold text-xl tracking-tight">
+              <img src={logo} className="h-20   w-auto"/>
+          </span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden lg:block">
+          <div className="flex items-center">
+            <Link
+              to="/"
+              className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            >
+              Home
+            </Link>
+            <Link
+              to="aboutus"
+              className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            >
+              About Us
+            </Link>
+            <Link
+              to="tripcreate"
+              className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white"
+            >
+              Create Trip
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
